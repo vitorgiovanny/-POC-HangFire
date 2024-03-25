a61a70601e231f59d7b877ec0f7a363e3a730331
Vitor Giovanny <vitorgiovannyinvis@gmail.com>
2024-03-24 21:37:11 -0300

    Adicionar arquivos de projeto.

---
+++ HangFirePoc/Program.cs
@@ -0,0 +1,9 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using Hangfire;
+using PocRecurringJob.Jobs;
+
+GlobalConfiguration.Configuration.UseSqlServerStorage(@"");
+
+RecurringJobs jobs = new RecurringJobs();
+jobs.ExecuteJob();
---
+++ PocRecurringJob/Jobs/RecurringJobs.cs
@@ -0,0 +1,27 @@
+﻿using Hangfire;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PocRecurringJob.Jobs
+{
+    public class RecurringJobs
+    {
+
+        public RecurringJobs() { }
+
+        public void ExecuteJob()
+        {
+            using (var server = new BackgroundJobServer())
+            {
+                RecurringJob.AddOrUpdate(() => Console.WriteLine("Tarefa Iniciado"), Cron.Minutely);
+
+                Console.WriteLine("Tarefa recorrente agendada.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
